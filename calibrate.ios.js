a294ec116057eb07db01f252079072c619726612
Gabriel Garrett <gabe@robotics-unlimited.com>
2016-06-16 10:19:51 -0500

    Successfully connected to bluefruit

--- calibrate.ios.js
+++ calibrate.ios.js
@@ -5,6 +5,8 @@ import {
   Text,
 } from 'react-native';
 
+'use-strict';
+
 var DeviceMotion = require('./DeviceMotion.ios');
 //var BleManager = require('./BleManager')
 var noble = require('react-native-ble');
@@ -24,12 +26,83 @@ class Calibrate extends Component {
    }
 
   componentWillMount () {
+    noble.startScanning();
     noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
+    console.log("Scanning");
     noble.startScanning();
   } else {
+    console.log("NOT SCANNING");
     noble.stopScanning();
   }
+});
+
+noble.on('discover', function(peripheral) {
+if (peripheral.id === "1675F086-42C0-BDB1-8022-C25E451446AC" || peripheral.advertisement.localName === "Adafruit Bluefruit LE") {
+noble.stopScanning();
+
+console.log('peripheral with ID ' + peripheral.id + ' found');
+var advertisement = peripheral.advertisement;
+
+var localName = advertisement.localName;
+var txPowerLevel = advertisement.txPowerLevel;
+var manufacturerData = advertisement.manufacturerData;
+var serviceData = advertisement.serviceData;
+var serviceUuids = advertisement.serviceUuids;
+
+if (localName) {
+  console.log('  Local Name        = ' + localName);
+}
+
+if (txPowerLevel) {
+  console.log('  TX Power Level    = ' + txPowerLevel);
+}
+
+if (manufacturerData) {
+  console.log('  Manufacturer Data = ' + manufacturerData.toString('hex'));
+}
+
+if (serviceData) {
+  console.log('  Service Data      = ' + serviceData);
+}
+
+if (serviceUuids) {
+  console.log('  Service UUIDs     = ' + serviceUuids);
+}
+
+console.log();
+
+peripheral.connect(function(error) {
+    peripheral.discoverServices([], function(error, services) {
+      var serviceIndex = 0;
+
+      // async.whilst(
+      //   function () {
+      //     return (serviceIndex < services.length);
+      //   },
+      //   function(callback) {
+      //     var service = services[serviceIndex];
+      //     var serviceInfo = service.uuid;
+      //
+      //     if (service.name) {
+      //       serviceInfo += ' (' + service.name + ')';
+      //     }
+      //     console.log(serviceInfo);
+      //
+      //     service.discoverCharacteristics([], function(error, characteristics) {
+      //       var characteristicIndex = 0;
+      //
+      //     });
+      //   },
+      //   function (err) {
+      //     peripheral.disconnect();
+      //   }
+      // );
+    });
+  });
+}
+//explore(peripheral);
+//}
 });
     DeviceMotion.startDeviceMotionUpdates(1000/100, (data) => {
       //console.log(data.attitude);
